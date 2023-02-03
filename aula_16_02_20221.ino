56e18576298b5f8f574c51624d5fcf6dc3a03469
Jaine Sena <121972347+JaineSena@users.noreply.github.com>
2023-02-03 17:05:51 -0300

    Add files via upload

---
+++ aula_16_02_20221.ino
@@ -0,0 +1,160 @@
+//Meu primeiro programa em C++ para Arduino
+
+int ledVermelho = 13;
+int ledLaranja = 12;
+int ledAmarelo = 11;
+int ledVerde = 10;
+int ledAzul = 9;
+
+int ledVermelho2 = 8;
+int ledLaranja2 = 7;
+int ledAmarelo2 = 6;
+int ledVerde2 = 5;
+int ledAzul2 = 4;
+
+int ledVermelho3 = 3;
+int ledLaranja3 = 2;
+int ledAmarelo3 = 1;
+int ledVerde3 = 0;
+
+
+void setup()
+{
+  pinMode (13, OUTPUT); //Configura o pino 13 como saída
+  pinMode (12, OUTPUT); //Configura o pino 12 como saída
+  pinMode (11, OUTPUT); //Configura o pino 11 como saída
+  pinMode (10, OUTPUT); //Configura o pino 10 como saída
+  pinMode (9, OUTPUT); //Configura o pino 9 como saída
+  pinMode (8, OUTPUT); //Configura o pino 8 como saída
+  pinMode (7, OUTPUT); //Configura o pino 7 como saída
+  pinMode (6, OUTPUT); //Configura o pino 6 como saída
+  pinMode (5, OUTPUT); //Configura o pino 5 como saída
+  pinMode (4, OUTPUT); //Configura o pino 4 como saída
+  pinMode (3, OUTPUT); //Configura o pino 3 como saída
+  pinMode (2, OUTPUT); //Configura o pino 2 como saída
+  pinMode (1, OUTPUT); //Configura o pino 1 como saída
+  pinMode (0, OUTPUT); //Configura o pino 0 como saída
+ 
+
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH); //Envia nível lógico para o pino 13
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(13, LOW); //Envia nível lógico para o pino 13
+  
+  digitalWrite(12, HIGH); //Envia nível lógico para o pino 12
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(12, LOW); //Envia nível lógico para o pino 12
+  
+  digitalWrite(11, HIGH); //Envia nível lógico para o pino 11
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(11, LOW); //Envia nível lógico para o pino 11
+  
+  digitalWrite(10, HIGH); //Envia nível lógico para o pino 10
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(10, LOW); //Envia nível lógico para o pino 10
+  
+  digitalWrite(9, HIGH); //Envia nível lógico para o pino 9
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(9, LOW); //Envia nível lógico para o pino 9
+  
+  digitalWrite(8, HIGH); //Envia nível lógico para o pino 8
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(8, LOW); //Envia nível lógico para o pino 8
+  
+  digitalWrite(7, HIGH); //Envia nível lógico para o pino 7
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(7, LOW); //Envia nível lógico para o pino 7
+  
+  digitalWrite(6, HIGH); //Envia nível lógico para o pino 6		
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(6, LOW); //Envia nível lógico para o pino 6
+  
+  digitalWrite(5, HIGH); //Envia nível lógico para o pino 5	
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(5, LOW); //Envia nível lógico para o pino 5
+  
+  digitalWrite(4, HIGH); //Envia nível lógico para o pino 4
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(4, LOW); //Envia nível lógico para o pino 4
+  
+  digitalWrite(3, HIGH); //Envia nível lógico para o pino 3
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(3, LOW); //Envia nível lógico para o pino 3
+  
+  digitalWrite(2, HIGH); //Envia nível lógico para o pino 2
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(2, LOW); //Envia nível lógico para o pino 2
+  
+  digitalWrite(1, HIGH); //Envia nível lógico para o pino 1
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(1, LOW); //Envia nível lógico para o pino 1
+  
+  digitalWrite(0, HIGH); //Envia nível lógico para o pino 0
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(0, LOW); //Envia nível lógico para o pino 0
+  
+  digitalWrite(0, HIGH); //Envia nível lógico para o pino 0
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(0, LOW); //Envia nível lógico para o pino 0
+  
+  digitalWrite(1, HIGH); //Envia nível lógico para o pino 1
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(1, LOW); //Envia nível lógico para o pino 1
+  
+  digitalWrite(2, HIGH); //Envia nível lógico para o pino 2
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(2, LOW); //Envia nível lógico para o pino 2
+  
+  digitalWrite(3, HIGH); //Envia nível lógico para o pino 3
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(3, LOW); //Envia nível lógico para o pino 3
+  
+  digitalWrite(4, HIGH); //Envia nível lógico para o pino 4
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(4, LOW); //Envia nível lógico para o pino 4
+  
+  digitalWrite(5, HIGH); //Envia nível lógico para o pino 5	
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(5, LOW); //Envia nível lógico para o pino 5
+ 
+  digitalWrite(6, HIGH); //Envia nível lógico para o pino 6		
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(6, LOW); //Envia nível lógico para o pino 6
+  
+  digitalWrite(7, HIGH); //Envia nível lógico para o pino 7
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(7, LOW); //Envia nível lógico para o pino 7
+  
+  digitalWrite(8, HIGH); //Envia nível lógico para o pino 8
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(8, LOW); //Envia nível lógico para o pino 8
+    
+  digitalWrite(9, HIGH); //Envia nível lógico para o pino 9
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(9, LOW); //Envia nível lógico para o pino 9
+  
+  digitalWrite(10, HIGH); //Envia nível lógico para o pino 10
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(10, LOW); //Envia nível lógico para o pino 10
+  
+  digitalWrite(11, HIGH); //Envia nível lógico para o pino 11
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(11, LOW); //Envia nível lógico para o pino 11
+  
+  digitalWrite(12, HIGH); //Envia nível lógico para o pino 12
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(12, LOW); //Envia nível lógico para o pino 12
+  
+  digitalWrite(13, HIGH); //Envia nível lógico para o pino 13
+  delay(100); // Faz o LED acender e apagar
+  digitalWrite(13, LOW); //Envia nível lógico para o pino 13
+  
+
+
+  
+}
+
+
